26dbac1f607bb8818dab25888d5f9e40210db2dd
Viktor Kollegov <rocket22.yandex.ru@gmail.com>
2025-06-09 17:18:21 +0300

    liquibase fix

---
+++ transactions/src/main/kotlin/com/example/config/LiquibaseConfig.kt
@@ -0,0 +1,29 @@
+package com.example.config
+
+import com.zaxxer.hikari.HikariDataSource
+import liquibase.integration.spring.SpringLiquibase
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.sql.DataSource
+
+@Configuration
+class LiquibaseConfig {
+
+    @Bean
+    fun dataSource(): HikariDataSource {
+        val dataSource = HikariDataSource()
+        dataSource.jdbcUrl = "jdbc:postgresql://127.0.0.1:5432/resourcedb"
+        dataSource.username = "postgres"
+        dataSource.password = "postgres"
+        dataSource.driverClassName = "org.postgresql.Driver"
+        return dataSource
+    }
+
+    @Bean
+    fun liquibase(dataSource: DataSource): SpringLiquibase {
+        val liquibase = SpringLiquibase()
+        liquibase.dataSource = dataSource
+        liquibase.changeLog = "classpath:db/changelog/db.changelog-master.yaml"
+        return liquibase
+    }
+}
