b9876e5ea6d9afc2bb9f97eaad9b5dd7542962b5
Viktor Kollegov <rocket22.yandex.ru@gmail.com>
2025-06-09 18:05:03 +0300

    r2dbc-postgresql fix

--- transactions/build.gradle.kts
+++ transactions/build.gradle.kts
@@ -7,18 +7,18 @@ plugins {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux:3.3.0")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.0")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.liquibase:liquibase-core:4.24.0")
+    implementation("org.postgresql:postgresql:42.7.2")
     implementation(project(mapOf("path" to ":common")))
     compileOnly("org.projectlombok:lombok:1.18.30")
     annotationProcessor("org.projectlombok:lombok:1.18.30")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.6.0")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.postgresql:postgresql:42.7.2")
 
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-    implementation("io.r2dbc:r2dbc-postgresql:0.8.13.RELEASE")
+    implementation("org.postgresql:r2dbc-postgresql:1.0.7.RELEASE")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
--- transactions/src/main/kotlin/com/example/config/LiquibaseConfig.kt
+++ transactions/src/main/kotlin/com/example/config/LiquibaseConfig.kt
@@ -4,9 +4,11 @@ import com.zaxxer.hikari.HikariDataSource
 import liquibase.integration.spring.SpringLiquibase
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import javax.sql.DataSource
 
 @Configuration
+@Profile("!test")
 class LiquibaseConfig {
 
     @Bean
