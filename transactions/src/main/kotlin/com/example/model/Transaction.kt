d2f82f4f845cfbb5cf840365a21ba44222a7f012
Viktor Kollegov <rocket22.yandex.ru@gmail.com>
2025-06-09 23:07:10 +0300

    timestamp fix

--- transactions/src/main/kotlin/com/example/model/Transaction.kt
+++ transactions/src/main/kotlin/com/example/model/Transaction.kt
@@ -3,11 +3,13 @@ package com.example.model
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 import java.math.BigDecimal
+import java.time.ZonedDateTime
 
 @Table("transaction")
 data class Transaction(
         @Id
         val id: Long? = null,
         val accountId: Long,
-        val amount: BigDecimal
+        val amount: BigDecimal,
+        val timestamp: ZonedDateTime = ZonedDateTime.now()
 )
